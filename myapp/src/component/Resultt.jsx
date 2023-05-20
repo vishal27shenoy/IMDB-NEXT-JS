ee3177eaad695cc3280828838322f08a9f42e82b
vishal <vishaliscoding27@gmail.com>
2023-05-20 16:25:43 +0530

    done

--- myapp/src/component/Resultt.jsx
+++ myapp/src/component/Resultt.jsx
@@ -6,7 +6,7 @@ const Resultt = ({ result }) => {
     <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {result &&
         result.map((result) => {
-          return <Card result={result} />;
+          return <Card result={result} key={result.id} />;
         })}
     </div>
   );
